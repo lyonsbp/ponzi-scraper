ce97b282a7ed49ab5474af887a26752310586c91
Brandon Lyons <brandonl.4231@gmail.com>
2022-01-16 23:04:56 -0500

    update howrare scraper

--- scraper/howRare/pageScraper.js
+++ scraper/howRare/pageScraper.js
@@ -1,40 +1,41 @@
+function getURL(baseURL, page) {
+  return `${baseURL}/?page=${page}&ids=&sort_by=rank`;
+}
+
 const scraperObject = {
-  url: "https://howrare.is/smb/?page=0&ids=&sort_by=rank",
+  url: "https://howrare.is/smb",
   async scraper(browser) {
-    let page = await browser.newPage();
-    console.log(`Navigating to ${this.url}...`);
-    await page.goto(this.url);
+    const maxPage = 20;
+    const PAGE_TIMEOUT = 12000;
+    let currentPage = 0;
 
-    console.log("Scraping page...");
-    await page.waitForSelector(".nft-listing");
+    const allImageUrls = [];
+    while (currentPage < maxPage) {
+      let currentURL = getURL(this.url, currentPage);
 
-    let links = await page.$$eval("nav ul.pagination li a", links => {
-      return links.map(link => link.href);
-    });
-    links = new Set(links);
-    console.log(links);
-
-    const allImageUrls = await page.$$eval(".nft-listing a div img", images => {
-      console.log(images);
-      return [images.map(image => image.src)];
-    });
-
-    for (const link of links) {
-      await new Promise(function (resolve) {
-        setTimeout(resolve, 16000 + Math.random() * 100);
-      });
       const newPage = await browser.newPage();
-      await newPage.goto(link);
+      await newPage.setDefaultNavigationTimeout(0);
+      console.log(`Navigating to ${currentURL}...`);
+
+      await newPage.goto(currentURL);
+      await newPage.waitForSelector(".featured_item_img");
+      console.log("Scraping page...");
 
       const imageUrls = await newPage.$$eval(
-        ".nft-listing a div img",
+        ".featured_item_img a img",
         images => {
           console.log(images);
           return images.map(image => image.src);
         }
       );
       allImageUrls.push(imageUrls);
-      console.log("Finished scraping page", link);
+
+      console.log("Finished scraping page", currentURL, allImageUrls.length);
+      currentPage++;
+
+      await new Promise(function (resolve) {
+        setTimeout(resolve, PAGE_TIMEOUT + Math.random() * 100);
+      });
     }
 
     console.log(allImageUrls.length, allImageUrls);
